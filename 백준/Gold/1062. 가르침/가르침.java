47c732a0f8bb0ff87ba0a6199889696a3b96bb00
WonBin <tray024400@gmail.com>
2025-09-15 10:32:43 +0900

    [Gold IV] Title: 가르침, Time: 128 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,66 +1,66 @@
 import java.util.*;
 import java.io.*;
-/*
-1062번 가르침
-*/
-public class Main{
+
+public class Main {
     static int N, K;
-    static String[] words;
-    static int count = 0;
-    static boolean[] visited = new boolean[26];
+    static int[] wordMasks;
+    static int answer = 0;
+    static int essentialMask;
 
-    public static void search(int idx, int depth) {
-        if (depth == K - 5) {
-            int readable = 0;
-            for (int i = 0; i < N; i++) {
-                boolean canRead = true;
-                String word = words[i];
-                for (int j = 0; j < word.length(); j++) {
-                    char c = word.charAt(j);
-                    if (!visited[c - 'a']) {
-                        canRead = false;
-                        break;
-                    }
-                }
-                if (canRead) readable++;
-            }
-            count = Math.max(count, readable);
-            return;
-        }
-        for (int i = idx; i < 26; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                search(i + 1, depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        words = new String[N];
+
+        wordMasks = new int[N];
+
         for (int i = 0; i < N; i++) {
-            words[i] = br.readLine();
+            String word = br.readLine();
+            int mask = 0;
+            for (int j = 0; j < word.length(); j++) {
+                mask |= (1 << (word.charAt(j) - 'a'));
+            }
+            wordMasks[i] = mask;
         }
 
-        visited['a' - 'a'] = true;
-        visited['n' - 'a'] = true;
-        visited['t' - 'a'] = true;
-        visited['i' - 'a'] = true;
-        visited['c' - 'a'] = true;
+        // 필수 알파벳 5개
+        essentialMask = 0;
+        for (char c : new char[]{'a','n','t','i','c'}) {
+            essentialMask |= (1 << (c - 'a'));
+        }
 
         if (K < 5) {
             System.out.println(0);
-        } else if (K == 26) {
-            System.out.println(N);
-        } else {
-            search(0, 0);
-            System.out.println(count);
+            return;
         }
-        return;
+        if (K == 26) {
+            System.out.println(N);
+            return;
+        }
+
+        search(0, 0, essentialMask);
+        System.out.println(answer);
+    }
+
+    static void search(int idx, int count, int learnedMask) {
+        if (count == K - 5) {
+            int readable = 0;
+            for (int mask : wordMasks) {
+                if ((mask & ~learnedMask) == 0) { // learnedMask가 wordMask를 모두 포함
+                    readable++;
+                }
+            }
+            answer = Math.max(answer, readable);
+            return;
+        }
+
+        for (int i = idx; i < 26; i++) {
+            if ((essentialMask & (1 << i)) != 0) continue; // 이미 필수 알파벳은 건너뜀
+            if ((learnedMask & (1 << i)) == 0) {
+                search(i + 1, count + 1, learnedMask | (1 << i));
+            }
+        }
     }
 }
