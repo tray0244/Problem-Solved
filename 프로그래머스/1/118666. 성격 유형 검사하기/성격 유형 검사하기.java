d736c3ac456690751c89cde22266d6bb003017af
WonBin <tray024400@gmail.com>
2025-09-16 16:24:25 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.07 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,23 +1,22 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     static Map<Character, Integer> score = new HashMap<>();
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
-        for(char c : new char[]{'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'}){
-            score.put(c, 0);
-        }
+        
+       for(char c : new char[]{'R', 'T', 'F', 'C', 'M', 'J', 'A', 'N'}){
+           score.put(c, 0);
+       }
         
         for(int i = 0; i < survey.length; i++){
             char left = survey[i].charAt(0);
             char right = survey[i].charAt(1);
-            int choice = choices[i];
+            int num = choices[i];
             
-            if(choice < 4){
-                score.put(left, score.get(left) + (4 - choice));
-            } else if(choice > 4){
-                score.put(right, score.get(right) + (choice - 4));
+            if(num < 4){
+                score.put(left, score.get(left) + (4 - num));
+            }else{
+                score.put(right, score.get(right) + (num - 4));
             }
         }
         StringBuilder result = new StringBuilder();
@@ -25,7 +24,7 @@ class Solution {
         result.append(score.get('C') >= score.get('F') ? 'C' : 'F');
         result.append(score.get('J') >= score.get('M') ? 'J' : 'M');
         result.append(score.get('A') >= score.get('N') ? 'A' : 'N');
-
+        
         return result.toString();
     }
 }
