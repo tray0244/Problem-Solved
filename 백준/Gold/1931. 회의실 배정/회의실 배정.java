5a1d065629395b866acdbcb329e7d6c3e0cf4310
WonBin <tray024400@gmail.com>
2025-09-24 10:25:33 +0900

    [Gold V] Title: 회의실 배정, Time: 504 ms, Memory: 43320 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,39 +1,59 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-public class Main{
+class Room implements Comparable<Room>{
+    int stime;
+    int etime;
+    public Room(int stime, int etime){
+        super();
+        this.stime = stime;
+        this.etime = etime;
+    }
+    @Override
+    public int compareTo(Room o){
+        // 종료 타임을 기준으로 오름 차순 정렬
+        int time = etime - o.etime;
+        // 종료 타임이 동일한 경우 시작 타입으로 오름 차순 정렬
+        if(time == 0){
+            time = stime - o.etime;
+        }
+        return time;
+    }
+}
+public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<int[]> list = new ArrayList<>();
+        // 입력 받고
         int N = Integer.parseInt(br.readLine());
+        ArrayList<Room> list = new ArrayList<>(N);
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int I = Integer.parseInt(st.nextToken());
-            int J = Integer.parseInt(st.nextToken());
-            list.add(new int[]{I, J});
+            list.add(new Room(Integer.parseInt(st.nextToken()),
+                              Integer.parseInt(st.nextToken())));
         }
-        list.sort((a, b) -> {
-            if(a[1] == b[1]) return a[0] - b[0];
-            return a[1] - b[1];
-        });
-        Queue<int[]> q = new LinkedList<>(list);
 
-        int[] first = q.poll();
-        int s = first[0];
-        int e = first[1];
-        int count = 1;
+        // 종료 타임을 기준으로 오름 차순으로 정렬
+        Collections.sort(list);
 
-        while(!q.isEmpty()){
-            int[] room = q.poll();
-            int start = room[0];
-            int end = room[1];
+        // 가장 빨리 끝나는 첫 회의를 시작 시켜준다. ==> endTime
+        int endTime = list.get(0).etime;
+        int cnt = 1;
 
-            if(e <= start){
-                count++;
-                e = end;
+        // 전체 회의를 반복 돌면서
+        for (int i = 1, size = list.size(); i < size; i++) {
+            // 회의 가능한지 비교 - 이전 회의의 종료 시간과 다음 회의 시작시간을 비교
+            if(endTime <= list.get(i).stime){
+                // 카운트 증가
+                cnt++;
+                // endTime 갱신
+                endTime = list.get(i).etime;
             }
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
