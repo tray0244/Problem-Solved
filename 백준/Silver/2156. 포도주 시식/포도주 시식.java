78915a945a7b4d8f54255419a487e9bdf875f309
WonBin <tray024400@gmail.com>
2025-10-28 19:02:05 +0900

    [Silver I] Title: 포도주 시식, Time: 132 ms, Memory: 14932 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] wine = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (n == 1) {
+            System.out.println(wine[1]);
+            return;
+        } else if (n == 2) {
+            System.out.println(wine[1] + wine[2]);
+            return;
+        }
+
+        int[] dp = new int[n + 1];
+
+        dp[1] = wine[1];
+        dp[2] = wine[1] + wine[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(
+                dp[i - 1],
+                Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i])
+            );
+        }
+
+        System.out.println(dp[n]);
+    }
+}
