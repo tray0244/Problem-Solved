942d295c7ef23c379133db32692f2420c7e32401
WonBin <tray024400@gmail.com>
2025-10-29 19:12:51 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18292 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        if (n >= 2) dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+
