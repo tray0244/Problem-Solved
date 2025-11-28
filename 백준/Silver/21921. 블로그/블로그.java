160df9a9160ef32ef781c6a97268703110f7aa52
WonBin <tray024400@gmail.com>
2025-11-28 17:30:50 +0900

    [Silver III] Title: 블로그, Time: 324 ms, Memory: 35176 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < X; i++) {
+            sum += arr[i];
+        }
+
+        int max = sum;
+        int count = 1;
+        
+        for (int i = X; i < N; i++) {
+            // 핵심 로직: sum - (이전 윈도우의 맨 앞 값) + (새로운 값)
+            sum = sum - arr[i - X] + arr[i];
+
+            if (sum > max) {
+                max = sum;
+                count = 1;
+            } else if (sum == max) {
+                count++;
+            }
+        }
+
+        if (max == 0) {
+            System.out.println("SAD");
+        } else {
+            System.out.println(max);
+            System.out.println(count);
+        }
+    }
+}
