00f22304aac363dcc0d7a717dcaafc637d36ce6e
WonBin <tray024400@gmail.com>
2025-12-05 13:30:30 +0900

    [Gold V] Title: 동전 2, Time: 132 ms, Memory: 14668 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int min = Integer.MAX_VALUE;
+
+        int[] dp = new int[100001];
+        int[] coin = new int[N];
+        for(int i = 0; i < N; i++){
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        
+        for(int i = 1; i < 100001; i++){
+            dp[i] = 100001;
+        }
+        dp[0] = 0;
+
+        for(int i = 0; i <= K; i++){
+            for(int c : coin){
+                if(i - c >= 0){
+                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[K] == 100001 ? -1 : dp[K]);
+    }
+}
