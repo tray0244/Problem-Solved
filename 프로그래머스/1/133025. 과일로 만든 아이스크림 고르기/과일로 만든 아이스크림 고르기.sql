c8c3265185908db8983eed05fbde17db661ebec7
WonBin <tray024400@gmail.com>
2025-12-18 18:43:29 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+select a.flavor
+from first_half a
+join icecream_info b on a.flavor = b.flavor
+where (a.total_order > 3000) and (b.ingredient_type = 'fruit_based')
+order by a.total_order desc
