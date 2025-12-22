faa12760a5d6eb5ee9ce9d898dd5aaf38ded8ece
WonBin <tray024400@gmail.com>
2025-12-22 17:08:52 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,11 @@
+select DATE_FORMAT(sales_date, '%Y-%m-%d') as sales_date, product_id, user_id, sales_amount
+from online_sale
+where sales_date >= '2022-03-01' and sales_date < '2022-04-01'
+
+UNION ALL
+
+select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, 
+    null as user_id, sales_amount
+from offline_sale
+where sales_date >= '2022-03-01' and sales_date < '2022-04-01'
+order by sales_date asc, product_id asc, user_id asc;
