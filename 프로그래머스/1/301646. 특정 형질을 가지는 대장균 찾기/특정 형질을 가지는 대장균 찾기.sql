25560bf501693f0ae5d7632beced60927b4b27ce
WonBin <tray024400@gmail.com>
2025-12-29 08:41:30 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,4 @@
 select count(*) as count
-from ecoli_data a
-where 1 = 1 and (genotype & 2) != 2 and ((genotype & 4) = 4 or (genotype & 1) = 1)
+from ecoli_data
+where (genotype & (1 | 4)) > 0
+  and (genotype & 2) = 0;
