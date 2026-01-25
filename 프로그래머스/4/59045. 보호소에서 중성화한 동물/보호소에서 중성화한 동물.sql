c9dd737bb05964fd57354e889bee2be4aae7e185
WonBin <tray024400@gmail.com>
2026-01-25 22:08:04 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+select i.animal_id, i.animal_type, i.name
+from animal_ins i
+join animal_outs o on i.animal_id = o.animal_id
+where 
+    i.sex_upon_intake like 'intact%'
+    and (o.sex_upon_outcome like 'spayed%' or o.sex_upon_outcome like 'neutered%')
+order by i.animal_id;
