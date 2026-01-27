d2b9a700d36a070139c02f8c631c1daad3950756
WonBin <tray024400@gmail.com>
2026-01-27 22:37:28 +0900

    [Silver I] Title: 주지수, Time: 596 ms, Memory: 111272 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N + 1][M + 1];
+
+        for(int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= M; j++){
+                int value = Integer.parseInt(st.nextToken());
+
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1] + value;
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < K; i++){
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1];
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
