20b9c26f5ec62e4bb23e8402c1454db6527f49d6
WonBin <tray024400@gmail.com>
2026-02-01 12:10:13 +0900

    [level 3] Title: 순위, Time: 11.84 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        int[][] graph = new int[n + 1][n + 1];
+
+        for (int[] r : results) {
+            graph[r[0]][r[1]] = 1;
+            graph[r[1]][r[0]] = -1;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                        graph[j][i] = -1;
+                    }
+                    if (graph[i][k] == -1 && graph[k][j] == -1) {
+                        graph[i][j] = -1;
+                        graph[j][i] = 1;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            
+            for (int j = 1; j <= n; j++) {
+                if (graph[i][j] != 0) count++;
+            }
+            if (count == n - 1) answer++;
+        }
+
+        return answer;
+    }
+}
