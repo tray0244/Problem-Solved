baee16e1695bb94a2e5c7198c4b03453c66ffb51
WonBin <tray024400@gmail.com>
2026-02-01 22:04:45 +0900

    [Gold IV] Title: LCS, Time: 112 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+
+        int N = str1.length;
+        int M = str2.length;
+
+        int[][] dp = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    // 상황 1: 문자가 같으면 대각선 값 + 1
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[N][M]);
+    }
+}
