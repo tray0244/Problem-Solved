16b01fd6a02ee7ecbf40910aaa3d034cc8a560e6
WonBin <tray024400@gmail.com>
2026-02-28 10:41:06 +0900

    [Gold V] Title: 입국심사, Time: 296 ms, Memory: 23968 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,12 +1,39 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        long[] t = new long[n];
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+            t[i] = Long.parseLong(br.readLine());
+            if (t[i] > max) max = t[i];
+        }
+
+        long l = 0;
+        long r = max * m;
+        long ans = r;
+
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            long cnt = 0;
+            for (int i = 0; i < n; i++) {
+                cnt += mid / t[i];
+                if (cnt >= m) break;
+            }
+            if (cnt >= m) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
