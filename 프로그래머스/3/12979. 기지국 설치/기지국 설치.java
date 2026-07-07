0db1b7ab8283dc8704df29de12cc4c7bec490b00
WonBin <tray024400@gmail.com>
2026-07-07 20:43:01 +0900

    [level 3] Title: 기지국 설치, Time: 0.47 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,22 +1,23 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int range = 2 * w + 1;
-        int prevEnd = 0;
+        int range = (2 * w) + 1;
+        int prev = 0;
         
         for(int station : stations){
             int start = station - w;
-            int gap = start - prevEnd - 1;
+            int gap = start - prev - 1;
             if(gap > 0){
                 answer += (gap + range - 1) / range;
             }
-            prevEnd = Math.max(prevEnd, station + w);
+            prev = Math.max(prev, station + w);
         }
-        int lastGap = n - prevEnd;
+        
+        int lastGap = n - prev;
         if(lastGap > 0){
             answer += (lastGap + range - 1) / range;
         }
-        
+
         return answer;
     }
 }
